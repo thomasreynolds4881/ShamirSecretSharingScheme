d6aa8131d6eed46c9a6073bee34921b74f8abeb0
Thomas Reynolds <48138681+thomasreynolds4881@users.noreply.github.com>
2020-01-03 00:39:57 -0600

    String generator is more random
    Generates separate strings with spread out characters rather than split by clumps, thus making it much harder/impossible to decipher longer passwords.

--- CreatePass.py
+++ CreatePass.py
@@ -1,14 +1,15 @@
 import math
-from random import shuffle
+import random
 
 def generateList(f, s):
 	slist = []
 	if s != 0:
-		num = math.ceil(len(f)/s)
+		num = math.ceil(len(f)/int(s))
 	else:
 		slist.append(f)
 		return slist
 
+	'''
 	tempstr = ""
 	midpos = 0
 	end = len(f)
@@ -33,18 +34,50 @@ def generateList(f, s):
 		tempstr = ""
 		midpos += num
 	shuffle(slist)
-	return slist
+	'''
+
+	# pick indexes at random from list of nums
+	numlist = []
+	for i in range(len(f)): # generate num list
+		numlist.append(i)
+
+	stringlist = []
+	templist = []
+	while len(numlist) > 0:
+		print("num:",num)
+		for i in range(num):
+			print("len of numlist:", len(numlist))
+			if len(numlist) != 0:
+				templist.append(numlist.pop(random.randint(0,len(numlist)-1))) #grab a random number and put it on the templist
+		slist.append(templist)
+		templist = []
+
+	# sort each list
+	for lists in slist:
+		lists.sort()
+	
+	# assign each val to a password char
+	finallist = []
+	for nums in slist: # go through each numlist comparing them to each char in f, if index isn't in numlist then replace with an '_'
+		for letter in range(len(f)):
+			if not letter in nums:
+				templist.append("_")
+			else:
+				templist.append(f[letter])
+		finallist.append("".join(templist))
+		templist = []
+	return finallist
 
 def main():
 	fullpass = input("Input secret word: ")
 	splitnum = int(input("How many splits? "))
 
 	splitlist = generateList(fullpass, splitnum)
-	shuffle(splitlist)
-	f = open("pass.txt","w")
-	
+	fi = open("pass.txt","w")
+
 	for word in splitlist:
-		f.write(word+"\n")
-	print("Successfully sent to file")
+		fi.write(word)
+		fi.write("\n")
+	print("Sent to file (pass.txt)")
 
 main()
