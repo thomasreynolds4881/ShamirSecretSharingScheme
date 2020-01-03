713eb91693d554a872820215e0c2337517f4b67c
Thomas Reynolds <48138681+thomasreynolds4881@users.noreply.github.com>
2020-01-03 00:55:50 -0600

    Added warnings for incomplete/conflicting data
    If two strings have different characters at the same index, a conflict will be marked there with a '?'. A warning for an incomplete decipher has been added as well, marked by an '_'.

--- DecodePass.py
+++ DecodePass.py
@@ -2,20 +2,30 @@
 file = open('pass.txt','r')
 passlist = file.read().splitlines()
 file.close()
-print(passlist)
 
 # Create finalpass list
 finalpass = []
 for i in passlist[0]:
 	finalpass.append('_')
 
-# Update the finalpass list every time there's new data
+# Update the finalpass list every time there's a new character to replace a '_'
 j = 0
+conflict = False
 for i in passlist: # go through passlist
 	index = 0
 	for j in i: # go through each string in passlist
 		if j != '_':
-			finalpass[index] = j
+			if j == finalpass[index] or finalpass[index] == '_':
+				finalpass[index] = j
+			else:
+				conflict = True
+				finalpass[index] = '?'
 		index += 1
+
+if "_" in finalpass:
+	print("WARNING: unable to fully decipher")
+
+if conflict:
+	print("WARNING: conflicts in data have been found and are marked with \'?\'")
  
-print("".join(finalpass)) 
+print("".join(finalpass))
