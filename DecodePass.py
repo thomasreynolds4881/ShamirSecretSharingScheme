c17599719adc2fa70831abbb54f4a0011fe7ef4d
Thomas Reynolds <48138681+thomasreynolds4881@users.noreply.github.com>
2020-01-03 01:14:25 -0600

    Added warning messages
    Includes lengthconflict in which strings are different lengths, overlapconflict in which two chars share an index, and a warning if the decipher is impossible to fully complete.

--- DecodePass.py
+++ DecodePass.py
@@ -10,22 +10,34 @@ for i in passlist[0]:
 
 # Update the finalpass list every time there's a new character to replace a '_'
 j = 0
-conflict = False
+overlapconflict = False
+lengthconflict = False
 for i in passlist: # go through passlist
 	index = 0
 	for j in i: # go through each string in passlist
 		if j != '_':
-			if j == finalpass[index] or finalpass[index] == '_':
-				finalpass[index] = j
-			else:
-				conflict = True
-				finalpass[index] = '?'
+			try:
+				if j == finalpass[index] or finalpass[index] == '_':
+					finalpass[index] = j
+				else:
+					# warning happens when two chars share an index
+					conflict = True
+					finalpass[index] = '?'
+			except:
+				# warning happens if a string is longer than a previous string (but if it's shorter it can be resolved)
+				lengthconflict = True
+				break
+
 		index += 1
 
+# print warning messages
 if "_" in finalpass:
 	print("WARNING: unable to fully decipher")
 
-if conflict:
+if lengthconflict:
+	print("WARNING: inconsistent string lengths; result may be inaccurate")
+
+if overlapconflict:
 	print("WARNING: conflicts in data have been found and are marked with \'?\'")
  
 print("".join(finalpass))
